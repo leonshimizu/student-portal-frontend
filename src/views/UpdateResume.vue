ca4fbdbf26cfef242d6cb49f1b2eeac0ff7b6cf0
“leonshimizu” <lmshimizu@gmail.com>
2021-12-07 17:56:12 -0800

    edited update page to take inputs

--- src/views/UpdateResume.vue
+++ src/views/UpdateResume.vue
@@ -1,42 +1,20 @@
 <template>
   <div class="Resume">
     <h1>{{ message }}</h1>
-    <div v-for="student in students" v-bind:key="student.id">
-      <h2>{{ student.first_name }}</h2>
-      <h2> {{ student.last_name }}</h2>
-      <p>{{ student.email }}</p>
-      <p>{{ student.phone_number }}</p>
-      <p>{{ student.bio }}</p>
-      <p>{{ student.linkedin_url }}</p>
-      <p>{{ student.twitter_handle }}</p>
-      <p>{{ student.website_url }}</p>
-      <p>{{ student.resume_url }}</p>
-      <p>{{ student.github_url }}</p>
-      <p>{{ student.photo }}</p>
-    </div>
-    <div v-for="experience in experiences" v-bind:key="experience.id">
-      <p>Start Date: {{ experience.start_date }}</p>
-      <p>End Date: {{ experience.end_date }}</p>
-      <p>Job Title: {{ experience.job_title }}</p>
-      <p>Company Name: {{ experience.company_name }}</p>
-      <p>Details: {{ experience.details }}</p>
-    </div>
-    <div v-for="education in educations" v-bind:key="education.id">
-      <p>Start Date: {{ education.start_date }}</p>
-      <p>End Date: {{ education.end_date }}</p>
-      <p>Degree: {{ education.degree }}</p>
-      <p>University Name: {{ education.university_name }}</p>
-      <p>Details: {{ education.details }}</p>
-    </div>
-    <div v-for="skill in skills" v-bind:key="skill.id">
-      <p>{{ skill.skill_name }}</p>
-    </div>
-    <div v-for="capstone in capstones" v-bind:key="capstone.id">
-      <p>{{ capstone.name }}</p>
-      <p>{{ capstone.description }}</p>
-      <p>{{ capstone.url }}</p>
-      <p>{{ capstone.screenshot }}</p>
-    </div>
+
+    <p>first_name: <input type="text" v-model="editUserParams.first_name"></p>
+    <p>first_name: <input type="text" v-model="editUserParams.first_name"></p>
+    <p>email: <input type="text" v-model="editUserParams.email"></p>
+    <p>phone_number: <input type="text" v-model="editUserParams.phone_number"></p>
+    <p>bio: <input type="text" v-model="editUserParams.bio"></p>
+    <p>linkedin_url: <input type="text" v-model="editUserParams.linkedin_url"></p>
+    <p>twitter_handle: <input type="text" v-model="editUserParams.twitter_handle"></p>
+    <p>website_url: <input type="text" v-model="editUserParams.website_url"></p>
+    <p>resume_url: <input type="text" v-model="editUserParams.resume_url"></p>
+    <p>github_url: <input type="text" v-model="editUserParams.github_url"></p>
+    <p>photo: <input type="text" v-model="editUserParams.photo"></p>
+    <button v-on:click="updateFunction()">Update</button>
+
   </div>
 </template>
 
@@ -48,57 +26,19 @@ import axios from "axios";
     data: function () {
       return {
         message: "Update Your Resume!",
-        students: {},
-        experiences: {},
-        educations: {},
-        skills: {},
-        capstones: {}
+        editUserParams: {}
       };
     },
-    created: function () {
-      this.indexStudents();
-      this.indexExperiences();
-      this.indexEducations();
-      this.indexSkills();
-      this.indexCapstones();
-    },
+    created: function () {},
     methods: {
-      indexStudents: function () {
-        console.log("View student details");
-        axios.get("http://localhost:3000/students").then((response) => {
-          console.log(response.data);
-          this.students = response.data;
-        });
-      },
-      indexExperiences: function () {
-        console.log("View experiences");
-        axios.get("http://localhost:3000/experiences").then((response) => {
-          console.log(response.data);
-          this.experiences = response.data;
-        });
-      },
-      indexEducations: function () {
-        console.log("View education");
-        axios.get("http://localhost:3000/educations").then((response) => {
-          console.log(response.data);
-          this.educataions = response.data;
-        });
-      },
-      indexSkills: function () {
-        console.log("View Skills");
-        axios.get("http://localhost:3000/Skills").then((response) => {
-          console.log(response.data);
-          this.skills = response.data;
-        });
-      },
-      indexCapstones: function () {
-        console.log("View Capstone");
-        axios.get("http://localhost:3000/capstones").then(
-          (response) => {
+      updateFunction: function() {
+        console.log("in the update function");
+        axios 
+          .patch(``, this.editUserParams)
+          .then(response => {
             console.log(response.data);
-            this.capstones = response.data;
-        });
-      },
+          })
+      }
     },
   };
 </script>
